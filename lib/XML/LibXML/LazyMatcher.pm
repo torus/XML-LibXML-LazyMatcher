db63f7dd485ef29f9c5e5859aba599bbcef06fbf
Toru Hisai <toru@kokohead.(none)>
2010-06-09 16:40:10 -1000

    just started

---
+++ lib/XML/LibXML/LazyMatcher.pm
@@ -0,0 +1,110 @@
+package XML::LibXML::LazyMatcher;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+XML::LibXML::LazyMatcher - The great new XML::LibXML::LazyMatcher!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use XML::LibXML::LazyMatcher;
+
+    my $foo = XML::LibXML::LazyMatcher->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Toru Hisai, C<< <toru at torus.jp> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-xml-libxml-lazymatcher at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-LibXML-LazyMatcher>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc XML::LibXML::LazyMatcher
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=XML-LibXML-LazyMatcher>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/XML-LibXML-LazyMatcher>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/XML-LibXML-LazyMatcher>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/XML-LibXML-LazyMatcher/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Toru Hisai.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of XML::LibXML::LazyMatcher
