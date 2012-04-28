aeffadd7e3a5ec76213f84ee779ce082852052c8
Toru Hisai <toru@torus.jp>
2012-04-29 01:20:35 +0900

    versin bump

--- lib/XML/LibXML/LazyMatcher.pm
+++ lib/XML/LibXML/LazyMatcher.pm
@@ -11,11 +11,11 @@ XML::LibXML::LazyMatcher - A simple XML matcher with lazy evaluation.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
